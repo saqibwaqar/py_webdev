01606006e29964cdd66eb5e8d579b810b2265a3f
saqibwaqar <saqibwaqarmufti@gmail.com>
2024-06-04 16:02:52 +0500

    blog app

--- Bootstrap Blog/main.py
+++ Bootstrap Blog/main.py
@@ -1,11 +1,33 @@
+import requests
 from flask import Flask, render_template
 
+NPOINT_URL = "https://api.npoint.io/77df45bbba34b98349f4"
+
 app = Flask(__name__)
+
+response = requests.get(NPOINT_URL)
+response.raise_for_status()
+data = response.json()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", blog_posts=data)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/post/<int:pid>")
+def post(pid):
+    return render_template("post.html", post=data[pid - 1])
 
 
 if __name__ == "__main__":
