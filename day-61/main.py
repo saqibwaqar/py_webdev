af068f4041e2a38567c648f09ec4bc59e572491e
saqibwaqar <saqibwaqarmufti@gmail.com>
2024-06-06 20:47:58 +0500

    contact forms addition

---
+++ day-61/main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms.fields.simple import StringField
+from wtforms.validators import DataRequired
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+
+
+app = Flask(__name__)
+app.secret_key = "some secret string"
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    login_form = LoginForm()
+    # print(request.method)
+
+    if request.method == "POST":
+        return render_template('login.html')
+    else:
+        return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
