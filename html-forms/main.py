2058adcfb63499f3bf750202f35b435c02709752
saqibwaqar <saqibwaqarmufti@gmail.com>
2024-06-06 09:17:00 +0500

    contact forms

---
+++ html-forms/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    name = ""
+    password = ""
+    if request.method == "POST":
+        name = request.form["username"]
+        password = request.form["password"]
+
+    return render_template("login.html", name=name, password=password)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
