acc8dbbbebe4ee2c89be044b70c8eba8e0b9357c
saqibwaqar <saqibwaqarmufti@gmail.com>
2024-06-09 11:45:45 +0500

    Coffee and wifi project complete

--- day-62/main.py
+++ day-62/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, validators
+from wtforms.fields.choices import SelectField
 from wtforms.validators import DataRequired
 import csv
 
@@ -25,6 +26,17 @@ Bootstrap5(app)
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
+    location = StringField('Cafe Location on Google Maps(URL)', validators=[DataRequired(), validators.URL()])
+    opening_time = StringField('Opening Time e.g. 8AM', validators=[DataRequired()])
+    closing_time = StringField('Closing Time e.g. 5:30PM', validators=[DataRequired()])
+
+    coffee_rating = SelectField('Coffee Rating',
+                                choices=['☕️', '☕️☕️', '☕️☕️☕️', '☕️☕️☕️☕️', '☕️☕️☕️☕️☕️'], validators=[DataRequired()])
+    wifi_rating = SelectField('Wifi Strength Rating',
+                              choices=['✘', '💪', '💪💪', '💪💪💪', '💪💪💪💪️', '💪💪💪💪💪️'], validators=[DataRequired()])
+    power_outlet_rating = SelectField('Power Socket Availability',
+                                      choices=['✘', '🔌', '🔌🔌', '🔌🔌🔌', '🔌🔌🔌🔌', '🔌🔌🔌🔌🔌🔌'], validators=[DataRequired()])
+
     submit = SubmitField('Submit')
 
 
@@ -43,11 +55,16 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
         print("True")
+        write_string = f"\n{form.cafe.data},{form.location.data},{form.opening_time.data},{form.closing_time.data},{form.coffee_rating.data},{form.wifi_rating.data},{form.power_outlet_rating.data}"
+        with open('cafe-data.csv', mode="a", encoding='utf-8') as csv_file:
+            csv_file.write(write_string)
+        return redirect(url_for('cafes'))
+
     # Exercise:
     # Make the form write a new row into cafe-data.csv
     # with   if form.validate_on_submit()
@@ -62,7 +79,7 @@ def cafes():
 
         for row in csv_data:
             list_of_rows.append(row)
-    
+
     return render_template('cafes.html', cafes=list_of_rows)
 
 
